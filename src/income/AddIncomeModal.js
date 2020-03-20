afb0fcd6e69412580c1d788157cff20060293184
gandh99 <gandh99@hotmail.com>
2020-03-20 19:39:26 +0800

    Added AddIncomeModal, which is based off AddExpenseModal

---
+++ src/income/AddIncomeModal.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { Modal, Button, Form } from 'react-bootstrap'
+import '../components/modal.css'
+import '../forms/form.css'
+import RadioGroup from '../forms/RadioGroup'
+import { isValidName, isValidPositiveNumber, isValidPercentage, isValidOption } from '../forms/input-validation'
+
+export default function AddIncomeModal(props) {
+    // Incomes state
+    const [incomeName, setIncomeName] = React.useState('')
+    const [incomeAmount, setIncomeAmount] = React.useState(0)
+    const [incomeFrequency, setIncomeFrequency] = React.useState('')
+    const [incomePercentageChange, setIncomePercentageChange] = React.useState(0)
+
+    const submitIncomes = (e) => {
+        e.preventDefault()
+
+        // Check for errors
+        if (!isValidName(incomeName)
+            || !isValidPositiveNumber(incomeAmount)
+            || !isValidPercentage(incomePercentageChange)
+            || !isValidOption(incomeFrequency)) {
+            return
+        }
+
+        // Update incomes in parents
+        const incomesData = {
+            incomeName,
+            incomeAmount,
+            incomeFrequency,
+            incomePercentageChange,
+        }
+        props.setIncomes([
+            ...props.incomes, incomesData
+        ])
+        props.saveIncomesToApp([
+            ...props.incomes, incomesData
+        ])
+
+        // Clear modal state
+        clearModalState()
+
+        // Close modal
+        props.onHide()
+    }
+
+    const clearModalState = () => {
+        setIncomeName('')
+        setIncomeAmount(0)
+        setIncomeFrequency('')
+        setIncomePercentageChange(0)
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            className='modal'
+        >
+            <Modal.Header className='modal-header'>
+                <Modal.Title id="contained-modal-title-vcenter" className='modal-title'>
+                    Add Income
+                </Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={submitIncomes}>
+                <Modal.Body className='modal-body'>
+                    <Form.Group controlId="incomeName">
+                        <Form.Control
+                            className='form-text-input'
+                            type="text"
+                            placeholder="Enter name of income"
+                            onChange={(e) => setIncomeName(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="incomeAmount">
+                        <Form.Control
+                            className='form-text-input'
+                            type="text"
+                            placeholder="Enter income amount"
+                            onChange={(e) => setIncomeAmount(e.target.value)}
+                        />
+                    </Form.Group>
+                    <RadioGroup
+                        id={'incomeFrequency'}
+                        optionNames={['Monthly', 'Yearly']}
+                        optionValues={['month', 'year']}
+                        onRadioSelect={(frequency) => {
+                            setIncomeFrequency(frequency)
+                        }}
+                    />
+                    <Form.Group controlId="incomePercentageChange">
+                        <Form.Control
+                            className='form-text-input'
+                            type="text"
+                            placeholder="Enter percentage change"
+                            onChange={(e) => setIncomePercentageChange(e.target.value)}
+                        />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer className='modal-footer'>
+                    <Button type='submit' className='modal-button'>
+                        Add
+                    </Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    );
+}
--- src/income/Income.js
+++ src/income/Income.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import Container from '../components/Container'
+import UtilityBar from '../components/UtilityBar'
+import ButtonGroup from '../components/ButtonGroup'
+import AddButton from '../components/AddButton'
+import AddIncomeModal from './AddIncomeModal'
 
-export default function Income() {
+export default function Income(props) {
+    const [showModal, setShowModal] = React.useState(false)
+    const [incomes, setIncomes] = React.useState(props.incomes)
+
     return (
-        <div>
-            
-        </div>
+        <Container
+            utilityBar={
+                <UtilityBar
+                    pageTitle={'Incomes'}
+                    buttonGroup={
+                        <ButtonGroup
+                            button={
+                                <AddButton
+                                    text={'Income'}
+                                    setShowModal={setShowModal}
+                                />
+                            }
+                        />
+                    }
+                />
+            }
+            modal={
+                <AddIncomeModal
+                    show={showModal}
+                    onHide={() => setShowModal(false)}
+                    incomes={incomes}
+                    setIncomes={setIncomes}
+                    saveIncomesToApp={props.saveIncomesToApp}
+                />
+            }
+            // cardContainer={
+            //     <CardContainer
+            //         cards={
+            //             incomes.map(expense => (
+            //                 <ExpenseCard
+            //                     expense={expense}
+            //                 />
+            //             ))
+            //         }
+            //     />
+            // }
+        />
     )
 }
