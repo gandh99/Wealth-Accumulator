386c9d90032c81edf45cee3d99738f08267bc543
gandh99 <gandh99@hotmail.com>
2020-03-22 21:16:23 +0800

    Created banner for the sidebar

--- src/navigation/Sidebar.js
+++ src/navigation/Sidebar.js
@@ -22,10 +22,10 @@ export default class Sidebar extends Component {
                     className='sidebar-container'
                     style={sidebarContainerStyle}>
                     <div className='sidebar-header' style={menuStyle}>
-                        <div
-                            className='sidebar-logo'>
-                            Back
-                    </div>
+                        <div className='banner-block'></div>
+                        <div className='banner-bottom-wave'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2f2b3f" fill-opacity="1" d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,96C1120,75,1280,117,1360,138.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+                        </div>
                     </div>
                     <div className='sidebar-menu' style={menuStyle}>
                         <Link to='/overview' onClick={this.props.toggleSidebar.bind(this, false)}>
