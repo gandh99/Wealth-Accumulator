97bb1dbc3be787addd530441171044d6704400ef
gandh99 <gandh99@hotmail.com>
2020-03-23 22:09:51 +0800

    Updates

---
+++ precache-manifest.b8f84a067a3af25b6f79cbd9699b9f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f78dbbdb6f4f0bc9ed5d9ff55123b7c",
+    "url": "/Wealth-Accumulator/index.html"
+  },
+  {
+    "revision": "a80878bca7bc6427f053",
+    "url": "/Wealth-Accumulator/static/css/main.561c9f68.chunk.css"
+  },
+  {
+    "revision": "be773e6cc82681b645e2",
+    "url": "/Wealth-Accumulator/static/js/2.a5b7223a.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/Wealth-Accumulator/static/js/2.a5b7223a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80878bca7bc6427f053",
+    "url": "/Wealth-Accumulator/static/js/main.fcf45d4f.chunk.js"
+  },
+  {
+    "revision": "3796794688f8672d6990",
+    "url": "/Wealth-Accumulator/static/js/runtime-main.c7da4859.js"
+  }
+]);
